172cdc84e1e293960700c1dc2a09120a46abae55
Unknown <solanojorge77@gmail.com>
2017-09-16 10:54:59 -0600

    Optimizado

--- BubleSort.cpp
+++ BubleSort.cpp
@@ -31,44 +31,7 @@ int main()
 {
 
 
-/*
-	//int sa[SIZE] = {3,4,7,1,4};
-	clock_t tiempo_inicio, tiempo_final;
-	double segundos;
-	
 
-	
-
-	CrearArchivo();
-	int SIZE = 100;
-	int sa[SIZE];
-	
-		for (int i=0; i<SIZE; i++){
-			sa[i]=rand();
-		}
-
-		// test array
-		cout << "Unsorted array:" << endl;
-	  	//printArray(sa, SIZE);
-
-	  	tiempo_inicio = clock();
-		bubbleSort(sa,SIZE);
-		tiempo_final = clock();
-
-       	cout << "Sorted array:" << endl;
-		//printArray(sa, SIZE);
-
-		segundos = (double)( tiempo_final-tiempo_inicio) / CLOCKS_PER_SEC; //según que estes midiendo el tiempo en segundos es demasiado grande
-	
-	    cout<<endl<<endl<<endl<<"segundos: "<<segundos<<endl;
-
-
-		//
-		
-		guardar(segundos,SIZE);
-		cin.get(); // hold window open
-  		return EXIT_SUCCESS;
-*/
 		
 	
 	
@@ -76,15 +39,16 @@ int main()
   //int sa[SIZE] = {3,4,7,1,4};
 	clock_t tiempo_inicio, tiempo_final;
 	double segundos;
-	int maxElem=3000;
+	int maxElem=1000;
 	double arraySegundos[maxElem];
 	int arrayElementos[maxElem];
 	int sa[maxElem];//array con numeros aleatorios
 
+	
 	CrearArchivo();
 	//int SIZE = 10;
 	int cont=0;
-	for (int qElem=1;qElem<maxElem;qElem+=10){
+	for (int qElem=1;qElem<=maxElem;qElem+=10){
 		
 
 		for (int i=0; i<qElem; i++){ // crea maxElem numero de numeros aleatorios agregandolos al array
@@ -140,14 +104,6 @@ void CrearArchivo()
 		puts("No se pudo crear el archivo");
 	}
 }
-
-
-
-
-
-
-
-
 
 
 
